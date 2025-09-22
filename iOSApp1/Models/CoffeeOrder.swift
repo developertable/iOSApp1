e36c2206c4261798639e32989eca9c974ab4e99e
developertable <rahul.kurra1986@gmail.com>
2025-09-22 09:12:10 -0400

    Organized in folders

---
+++ iOSApp1/Models/CoffeeOrder.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrder.swift
+//  iOSApp1
+//
+//  Created by Rahul Kurra on 2025-09-20.
+//
+
+import Foundation
+
+import Foundation
+
+struct CoffeeOrder: Hashable {
+    var drinkType: String
+    var size: String
+    var milk: Bool
+    var sugar: Bool
+}
